3bd42a538685d963332a4af2faffb0438896f2ce
Eishika Chauhan <chauhaneishika@gmail.com>
2026-06-03 17:16:46 +0530

    Fibonacci Numbers

---
+++ src/FibonacciNumbers.java
@@ -0,0 +1,8 @@
+public class FibonacciNumbers {
+    public int nthFibonacci(int n) {
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        return nthFibonacci(n -1)+ nthFibonacci(n -2);
+    }
+}
