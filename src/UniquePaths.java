d175322240f205528b5c0c441d6ae1e9d2cb7898
Eishika Chauhan <chauhaneishika@gmail.com>
2026-06-04 18:18:25 +0530

    Unique Paths

---
+++ src/UniquePaths.java
@@ -0,0 +1,7 @@
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1) return 1;
+        if(m == 0 || n == 0) return 0;
+        return uniquePaths(m-1,n) + uniquePaths(m, n-1);
+    }
+}
