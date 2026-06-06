39d9b18dcacb48e90d1ef23e07502e9436781aaa
Eishika Chauhan <chauhaneishika@gmail.com>
2026-06-06 18:09:20 +0530

    Current Parenthesis

---
+++ src/CurrentParentheses.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrentParentheses {
+    public void generate(int n, int l, int r, String s,List<String> ans) {
+        if(r == n) { // base case 
+            ans.add(s);
+            return;
+        }
+        if(l < n) generate(n,l+1,r,s+"(",ans);
+        if(r < l) generate(n,l,r+1,s+")",ans);
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        generate(n,0,0,"",ans);
+        return ans;
+    }
+}
